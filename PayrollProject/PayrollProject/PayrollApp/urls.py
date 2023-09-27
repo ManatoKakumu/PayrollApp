6f668e7ceebc7b66cf421ad4e367acd26d49af4d
ManatoKakumu <tmnwbsabcdc@docomo.ne.jp>
2023-09-27 17:14:58 +0900

    画面遷移完了

--- PayrollProject/PayrollProject/PayrollApp/urls.py
+++ PayrollProject/PayrollProject/PayrollApp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from . import views
+from .views import (
+    WorkReportView, PayrollView, ShiftView, RegisterTeacherView, RegisterClassView, ContactView, OutputCSVView
+)
 from django.views.generic.base import TemplateView
 
 app_name = "payroll_app"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="display/home.html"), name="home"),
+    path("work_report/", WorkReportView.as_view(), name="work_report"),
+    path("payroll/", PayrollView.as_view(), name="payroll"),
+    path("shift/", ShiftView.as_view(), name="shift"),
+    path("register_teacher/", RegisterTeacherView.as_view(), name="register_teacher"),
+    path("register_class/", RegisterClassView.as_view(), name="register_class"),
+    path("contact/", ContactView.as_view(), name="contact"),
+    path("csv/", OutputCSVView.as_view(), name="csv"),
 ]
--- PayrollProject/PayrollProject/PayrollApp/views.py
+++ PayrollProject/PayrollProject/PayrollApp/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
-
+from django.views.generic.base import (
+    View,
+)
 # Create your views here.
+
+# 勤務報告画面
+class WorkReportView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "display/work_report.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# 給与計算画面
+class PayrollView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "display/payroll.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# シフト画面
+class ShiftView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "display/shift.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# 講師情報登録画面
+class RegisterTeacherView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "registration/teacher.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# 授業情報登録画面
+class RegisterClassView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "registration/class.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# 連絡発信画面
+class ContactView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "registration/contact.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
+
+# csv出力画面
+class OutputCSVView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "registration/csv.html")
+    
+    def post(self, request, *args, **kwargs):
+        pass
